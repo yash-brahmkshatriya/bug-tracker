63a7f7977fda25dccf75c12304ff56d0bc5108ee
Ayush Zaveri <ayushzaveri@gmail.com>
2021-03-18 10:34:20 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { Provider as StoreProvider } from 'react-redux';
-import { appTheme } from './theme';
-import { CssBaseline } from '@material-ui/core';
-import MainComponent from './components/MainComponent';
-import { ConfigureStore } from './redux/Store';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Provider as StoreProvider } from "react-redux";
+import { appTheme } from "./theme";
+import { CssBaseline } from "@material-ui/core";
+import MainComponent from "./components/MainComponent";
+import { ConfigureStore } from "./redux/Store";
 
 const store = ConfigureStore();
 
