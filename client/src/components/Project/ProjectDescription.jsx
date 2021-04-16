46e9f3611e50dbf568daccae38fbfa06878b6377
Yash Brahmkshatriya <yash311000@gmail.com>
2021-04-16 15:09:18 +0530

    Changed Project Layout

--- client/src/components/Project/ProjectDescription.jsx
+++ client/src/components/Project/ProjectDescription.jsx
@@ -1,43 +1,56 @@
 import React from 'react';
-
-import { Typography, Box, useMediaQuery, TextField } from '@material-ui/core';
+import {
+  Typography,
+  Box,
+  useMediaQuery,
+  TextField,
+  Paper,
+} from '@material-ui/core';
 import { useStyles as projDetStyles } from './projDetStyles';
 import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import { useStyles as threadStyles } from '../Threads/threadStyles';
 
 function ProjectDescription({ mode, editForm, description }) {
   const css = projDetStyles();
+  const threadCss = threadStyles();
   return (
-    <>
-      <Box display="flex" alignItems="center">
-        <DescriptionOutlinedIcon />
-        <Typography variant="h5" style={{ marginLeft: '8px' }}>
-          Description
-        </Typography>
-      </Box>
-      <Box className={css.timeNameInfo} style={{ paddingLeft: '4px' }}>
-        {mode === 'edit' ? (
-          <TextField
-            type="text"
-            fullWidth
-            multiline
-            rowsMax={5}
-            id="description"
-            value={editForm.values.description}
-            onChange={editForm.handleChange}
-            onBlur={editForm.handleBlur}
-            label="Project Description"
-            InputProps={{ disableUnderline: true }}
-          />
-        ) : (
-          <Typography
-            variant="body1"
-            style={{ marginTop: '8px', whiteSpace: 'pre-line' }}
-          >
-            {description}
+    <Paper
+      elevation={4}
+      className={threadCss.paperSection}
+      style={{ marginTop: '8px' }}
+    >
+      <Box p={2}>
+        <Box display="flex" alignItems="center">
+          <DescriptionOutlinedIcon />
+          <Typography variant="h5" style={{ marginLeft: '8px' }}>
+            Description
           </Typography>
-        )}
+        </Box>
+        <Box className={css.timeNameInfo} style={{ paddingLeft: '4px' }}>
+          {mode === 'edit' ? (
+            <TextField
+              type="text"
+              fullWidth
+              multiline
+              rowsMax={5}
+              id="description"
+              value={editForm.values.description}
+              onChange={editForm.handleChange}
+              onBlur={editForm.handleBlur}
+              label="Project Description"
+              InputProps={{ disableUnderline: true }}
+            />
+          ) : (
+            <Typography
+              variant="body1"
+              style={{ marginTop: '8px', whiteSpace: 'pre-line' }}
+            >
+              {description}
+            </Typography>
+          )}
+        </Box>
       </Box>
-    </>
+    </Paper>
   );
 }
 
